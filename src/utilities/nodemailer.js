5afe15f6ef9979ee603260b649c90cff4cd02c98
nkbtemmy <emmanuelnkubito2@gmail.com>
2022-05-26 15:36:37 +0200

    making email send by outlokk

--- src/utilities/nodemailer.js
+++ src/utilities/nodemailer.js
@@ -7,16 +7,18 @@ import nodemailer from 'nodemailer'
  */
 export const emailSender = async (options) => {
   try {
-    const transporter = nodemailer.createTransport({
-      host: process.env.TRANSPORTER_SERVICE,
-      port: 465,
-      auth: {
-        user: process.env.SERVICE_USERNAME,
-        pass: process.env.SERVICE_PASSWORD,
-      },
-      secure: false,
-      logger: true,
-      debug: true,
+    var transporter = nodemailer.createTransport({
+        host: process.env.TRANSPORTER_SERVICE, // hostname
+        service: 'outlook',
+        secureConnection: false, // TLS requires secureConnection to be false
+        port: 587, // port for secure SMTP
+        tls: {
+          rejectUnauthorized: false
+        },
+        auth: {
+          user: process.env.SERVICE_USERNAME,
+          pass: process.env.SERVICE_PASSWORD,
+        }
     });
     const mailOptions = {
       from: `Smart Parking <${process.env.SERVICE_USERNAME}>`,
