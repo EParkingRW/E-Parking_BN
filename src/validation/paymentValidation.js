92eff11a077eaefeb3203bd2e7797a4f630cb996
nkbtemmy <emmanuelnkubito2@gmail.com>
2022-06-02 11:59:51 +0200

    make it better thatn before

--- src/validation/paymentValidation.js
+++ src/validation/paymentValidation.js
@@ -4,11 +4,10 @@ import Response from '../helpers/Response';
 export default class PaymentValidation {
   static async momo(req, res, next) {
     const schema = Joi.object().keys({
-      phone_number: Joi.string().regex(/^\d{3}-\d{3}-\d{4}$/).required().messages({
+      phone_number: Joi.string().required().messages({
         'string.base': '{{#label}} must be a string',
         "string.empty": `{{#label}} cannot be an empty field`,
-        "any.required": `{{#label}} is a required.`,
-        "string.pattern.base":"{{#label}} must satisfy pattern like 555-555-5555"
+        "any.required": `{{#label}} is a required.`
     }),
       amount: Joi.string().required(),
     });
@@ -32,11 +31,10 @@ export default class PaymentValidation {
         expiry_year:Joi.string().required().max(2),
         currency:Joi.string().valid('RWF', 'USD').default('RWF'),
         amount:Joi.number().integer().required(),
-        phone_number: Joi.string().regex(/^\d{3}-\d{3}-\d{4}$/).required().messages({
+        phone_number: Joi.string().required().messages({
           'string.base': '{{#label}} must be a string',
           "string.empty": `{{#label}} cannot be an empty field`,
           "any.required": `{{#label}} is a required.`,
-          "string.pattern.base":"{{#label}} must satisfy pattern like 555-555-5555"
       })
     });
     const { error } = schema.validate(req.body);
